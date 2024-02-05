354d04ffc296edd72f234a7ad025f0899a8e3bcf
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2024-02-05 05:39:37 +0530

    Add files via upload

---
+++ DSA/insertion_sort.cpp
@@ -0,0 +1,12 @@
+//insertion sort
+
+//the funda here is
+//It always takes an element
+//and places it in its correct position
+
+
+//the array is 
+//14 9 15 12 6 8 13
+
+//the algorithm starts with looking at first element as the array
+//then u go to next element, so taken array size is 2 and ask the prev element(/where the second element should've been?)
