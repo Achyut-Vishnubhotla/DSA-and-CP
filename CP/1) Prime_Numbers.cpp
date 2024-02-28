3e125e6adfd5e44f5fd37437f9a0142baefd1d3c
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2024-02-28 21:57:04 +0530

    Add files via upload

---
+++ CP/1) Prime_Numbers.cpp
@@ -0,0 +1,41 @@
+/*
+some facts about prime numbers
+
+1)2 is the only even prime
+
+2)every prime can be written as 6n+1(or)6n-1(where n is a natural number), except 2 and 3
+for example:
+6 * 2 - 1 = 11(prime)
+
+3)Goldbach conjecture: Every even integer greater than 2
+can be expressed as the sum of 2 primes
+for example:
+13 + 3 = 16(even integer)
+
+4)2 and 3 are only consecutive numbers that are prime
+
+5)wilson's theorem => (p-1)! = (p-1) mod p
+(should be explained briefly)
+
+*/
+/*
+wilson's theorem is 
+(p-1)! = (p-1) % p
+
+now what is the significance of the theorem?
+
+if p=5
+then 
+(5-1)! = (5-1) % 5 
+    4! = 4 % 5
+    4! = 4
+ (25)%5 = 4
+    4 = 4   
+*/
+
+//what is prime number?
+//a number should be divisible by 1 & itself 
+//and no of factors of that number=2
+
+
+
