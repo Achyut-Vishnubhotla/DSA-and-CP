672780d69ca2c319ae840934adb847033730ddb3
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2024-02-29 18:37:00 +0530

    Update 1) Prime_Numbers.cpp

--- CP/1) Prime_Numbers.cpp
+++ CP/1) Prime_Numbers.cpp
@@ -35,7 +35,84 @@ then
 
 //what is prime number?
 //a number should be divisible by 1 & itself 
-//and no of factors of that number=2
+//and no of factors of that number is 2( 1 and that number)
+
+
+//so if the question is to check if a number is divisible by some number or not?
+
+//our approach will be -> n%i == 0  (where n is the number taken to be checked & i is some numbers used for checking)
+//here i can't be 1 and n
+//because condition to be prime is divisible by 1 and itself only
+//if v do that result might be the number is prime and composite
+
+//so what do v need to check is if the no of factors = 2 or not (to check the number is prime or not)
+
+//if there is a number n (to check if it is prime or not)
+//v loop from 1 to n and check the divisibility from 1 to n
+
+/*
+cnt=0
+for(i=1;i<=n;i++){
+    if(n%i==0){
+        cnt++
+    }
+}
+if(cnt==2) print(prime)
+else print(not prime)
+*/
+
+//here the time complexity is O(n) 
+//as the no of iterations the for loop takes is n times
+
+//this is not optimised version of the problem
+//v will come to that
+
+//so can v optimise it further? s v can 
+
+/*
+for a number like 
+     (i) (n/i)
+      |    |
+ 36 = 1 * 36
+      2 * 18
+      3 * 12
+      4 * 9
+      6 * 6
+    ---------
+      9 * 4
+     12 * 3
+     18 * 2
+     36 * 1
+
+for 36, the factors came after 6 * 6 is the repeatition 
+so if v iterate upto it squareroot so loop is trimmed
+*/
+
+/*
+cnt=0
+for(i=1;i<=sqrt(n);i++){
+    if(n%i==0){
+        if((n/i)!=i){
+            cnt++
+        }
+    }
+}
+if(cnt==0) print(prime)
+else print(not prime)
+*/
+//time complexity for this is O(sqrt(n) * log n)
+//because the <=sqrt(n) condition is checked everytime so for checking it takes log n
 
 
 
+//if the question is print the sum of factors
+/*
+for(i=1;i*i<=n;i++){
+    if(n%i==0){
+        sum+=i
+        if((n/i)!=i){
+            sum+=(n/i)
+        }
+    }
+}
+*/
