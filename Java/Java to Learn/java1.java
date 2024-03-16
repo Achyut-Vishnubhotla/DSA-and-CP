4baa0da66b5f67f72e9eae692ea5f3cff1b5edf9
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2024-03-16 19:39:22 +0530

    Add files via upload

---
+++ Java/Java to Learn/java1.java
@@ -0,0 +1,8 @@
+//printing hello world and the boiler plate in java
+public class java1{
+    public static void main(String args[]){
+        System.out.print("Hello World \n");
+        System.out.print("Hello World \n");
+        System.out.print("Hello World \n");
+    }
+}
